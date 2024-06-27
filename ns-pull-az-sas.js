fa076069cdc87d2c3e0e3a10172b8c2e7d2d8fb1
chrattar <cattar@protonmail.ch>
2024-06-27 19:31:31 -0400

    Update ns-pull-az-sas.js

--- ns-pull-az-sas.js
+++ ns-pull-az-sas.js
@@ -6,33 +6,32 @@ define(['N/https', 'N/file', 'N/task'], function(https, file, task) {
 
     function execute(context) {
         try {
-            var sasUrl = 'your_sas_url_here'; // Replace with the actual SAS URL
+            var sasUrl = 'azure.microsoft'; // Replace with the actual SAS URL
 
-            // Make a GET request to the SAS URL to download the file
+            // GET request -> SAS URL for dl
             var response = https.get({
                 url: sasUrl
             });
 
             if (response.code === 200) {
-                // Save the file to the File Cabinet
+                // Save to File Cabinet
                 var csvFile = file.create({
                     name: 'extracted_data.csv',
                     fileType: file.Type.CSV,
                     contents: response.body,
-                    folder: -15 // Replace with the ID of the target folder in the File Cabinet
-                });
+                    folder: -15 // File Cabinent which is storing all the requiste JS SuiteScripts
 
                 var fileId = csvFile.save();
                 log.debug('File saved to File Cabinet', 'File ID: ' + fileId);
 
-                // Create a CSV import task
+                // create task for import
                 var csvImportTask = task.create({
                     taskType: task.TaskType.CSV_IMPORT,
                     importFile: file.load({ id: fileId }),
-                    mappingId: 12345 // The ID of the saved import map
+                    mappingId: 44533 //PULL Internal ID for the custom import map that was created in NS
                 });
 
-                // Submit the CSV import task
+                // submit task for import
                 var taskId = csvImportTask.submit();
                 log.debug('CSV Import Task Submitted', 'Task ID: ' + taskId);
             } else {
